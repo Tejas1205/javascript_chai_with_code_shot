9878430e680d369a8bb4430604696196a7f6b59a
Tejas1205 <rakhundetejas461@gmail.com>
2025-02-17 22:54:55 +0530

    Added_datatype

---
+++ 02_datatype.js
@@ -0,0 +1,20 @@
+
+//this are premative data type
+let a=1
+let b=BigInt
+let c="string"
+let g=true
+let d=null
+let e;
+let f=Symbol("username")
+//number => 2 to power 53
+// bigint
+//string =>""
+//boolean => true/false
+//null => standlone value
+//undefined =>
+//symbol => unique
+
+console.log(typeof(null));///object
+console.log(typeof(undefined));//undefined
+
