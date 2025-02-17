2ffab2f7a6e5608074206ab49611fbc0d72ca018
Tejas1205 <rakhundetejas461@gmail.com>
2025-02-17 23:39:36 +0530

    conversionOprations

---
+++ 03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score="33"
+
+let valueInnumber=Number(score)
+console.log(typeof score); //string
+console.log(valueInnumber);//NaN - NOT A NUMBER
+
+let score2=true  //false -value=0 boolean
+let value=Number(score2)
+console.log(typeof score2);//boolean
+console.log(value);//1
+
+let score3=null
+let value1=Number(score3)
+console.log(typeof score3);
+console.log(value1);
+
+let score4=undefined
+let value4=Number(score4)
+console.log(score4);//undefined
+
+console.log(value4);//NaN
+
+let  LoggedIn='TEJAS'///TRUE, 1-TRUE , '' -FALSE  
+let value5= Boolean(LoggedIn)
+console.log(value5);
+
+
+let num=3
+let stringconvert=String(num)
+console.log(num);//3
+console.log(stringconvert);///3
+
+
+
+
+
+
+
+
