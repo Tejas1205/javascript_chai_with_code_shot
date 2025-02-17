a6aed4dd2bbaa1bc46a0ca5ec68f1834d1111633
Tejas1205 <rakhundetejas461@gmail.com>
2025-02-17 21:30:03 +0530

    Added 01_variable.js and 02_datatype.js

---
+++ 01_variable.js
@@ -0,0 +1,24 @@
+// 
+
+
+let nameid='tejas';
+const email='rakhunde@123.cpm'
+var phone="1010"
+
+address='indapur'
+
+// let nameid='dipak'
+// const email='rakhundetejas471@gmail.com'
+
+
+var phone='1010'
+
+console.table([nameid,email,phone])
+
+
+// console.log(nameid);
+// console.log(email);
+// console.log(phone);
+// console.log(address);
+
+
